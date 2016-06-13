9e2cb02b2bff2554bd248aaaad02e0f4d73239cd
Martín Basterrechea <martinbaste@users.noreply.github.com>
2016-06-13 23:32:29 +0200

    Update filter_list.py

--- filter_list.py
+++ filter_list.py
@@ -6,4 +6,7 @@ def filter_list(l):
 	return l
 
 filter_list([1,2,'a','b'])
+#The list becomes shorter every time you take out something from the list. 
+#I guess that alters the behavior of the for loop. 
+#In this case you could probably work around it. But I would create another list and add the number elements to them.
 
