ec8bbd0cf351a7c3c9dac0643acecb1810581657
linem <line.moller@gmail.com>
2016-06-14 19:41:19 +0200

    Update filter_list.py

--- filter_list.py
+++ filter_list.py
@@ -1,12 +1,17 @@
 
 def filter_list(l):
+	new_list = []
 	for element in l:
-		if element not in range(0,10):
-			l.remove(element)
-	return l
+		if type(element) == int:
+			new_list.append(element)
+	return new_list
 
 filter_list([1,2,'a','b'])
 #The list becomes shorter every time you take out something from the list. 
 #I guess that alters the behavior of the for loop. 
 #In this case you could probably work around it. But I would create another list and add the number elements to them.
+
+#Updated:
+#New list added for appending
+#And bug fixed, as numbers could be higher than 10.
 
